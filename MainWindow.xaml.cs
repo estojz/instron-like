b7b28a2f0cff14ea173bba9b9eb6f97c2da6f73c
estojz <estojz@gmail.com>
2026-04-15 12:04:29 +0300

    Update MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Timers;
 using System.Windows;
+using System.Collections.Generic;
 
 namespace InstronLike
 {
@@ -10,6 +11,9 @@ namespace InstronLike
         private double _time;
         private double _position;
         private double _force;
+
+        private List<double> _x = new();
+        private List<double> _y = new();
 
         public MainWindow()
         {
@@ -21,6 +25,12 @@ namespace InstronLike
             _time = 0;
             _position = 0;
             _force = 0;
+
+            _x.Clear();
+            _y.Clear();
+
+            Plot.Plot.Clear();
+            Plot.Refresh();
 
             _timer = new Timer(50);
             _timer.Elapsed += Loop;
@@ -35,14 +45,22 @@ namespace InstronLike
         private void Loop(object sender, ElapsedEventArgs e)
         {
             _time += 0.05;
+
             _position += 0.1;
             _force = _position * 10;
+
+            _x.Add(_position);
+            _y.Add(_force);
 
             Dispatcher.Invoke(() =>
             {
                 ForceText.Text = $"Force: {_force:F2} N";
                 PositionText.Text = $"Position: {_position:F2} mm";
                 TimeText.Text = $"Time: {_time:F2} s";
+
+                Plot.Plot.Clear();
+                Plot.Plot.Add.Scatter(_x.ToArray(), _y.ToArray());
+                Plot.Refresh();
             });
         }
     }
